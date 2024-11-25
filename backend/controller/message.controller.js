7d80b1e8107b2ee9f0cfc04ca5bdef4de09c0a8f
jassiyo <jaskirathsingh2@gmail.com>
2024-11-25 22:47:53 +0530

    user.route and controller created

--- backend/controller/message.controller.js
+++ backend/controller/message.controller.js
@@ -30,7 +30,7 @@ export const sendMessage = async (req, res)=>{
         res.status(200).json(newMessage)
 
     } catch (error) {
-        console.log("Error in sendMessage controller",error.message)
+        console.log("Error in sendMessage controller", error.message)
         res.status(500).json({error:"Internal server error (message controller!)"})
     }
 }
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 import express from 'express'
 import dotenv from "dotenv"
-import authRoutes from './Routes/auth.route.js'
-import messageRoute from './Routes/message.route.js'
-import connectToMongoDB from './Db/MongoDb.js';
 import cookieParser from 'cookie-parser';
+
+// Routes
+import authRoutes from './Routes/auth.route.js'
+import messageRoutes from './Routes/message.route.js'
+import userRoutes from './Routes/user.route.js'
+
+// Database connection
+import connectToMongoDB from './Db/MongoDb.js';
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,7 +21,8 @@ app.use(express.json());
 app.use(cookieParser())
 
 app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoute)
+app.use("/api/messages",messageRoutes)
+app.use("/api/users",userRoutes)
 
 // app.get('/',(req, res)=>{
 //     res.send("hello world")
